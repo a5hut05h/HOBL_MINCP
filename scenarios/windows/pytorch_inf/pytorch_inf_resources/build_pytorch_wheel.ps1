01344a69653a1e202758257d3085a73747e697ed
Phil Nachreiner <philnach@microsoft.com>
2026-05-05 12:06:41 -0700

    Remove extra build file

--- scenarios/windows/pytorch_inf/pytorch_inf_resources/build_pytorch_wheel.ps1
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-# This build script has been moved out of the HOBL scenario tree.
-# It is a one-time utility for producing custom PyTorch wheels and does not
-# run as part of the HOBL prep/run/teardown lifecycle.
-Write-Host "This build script has been relocated. It is not part of the HOBL scenario."
-Exit 0
