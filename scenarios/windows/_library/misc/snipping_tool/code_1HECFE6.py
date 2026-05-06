9246c236fc4ac8fb8c0cf99dddddc8928b789698
Ashutosh Das <dasashutosh@microsoft.com>
2026-05-06 16:48:00 +0530

    Update Snipping tool

--- scenarios/windows/_library/misc/snipping_tool/code_1HECFE6.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-import logging
-
-def run(scenario):
-    logging.debug('Executing code block: code_1HECFE6.py')
-    scenario._kill("SnippingTool.exe")
