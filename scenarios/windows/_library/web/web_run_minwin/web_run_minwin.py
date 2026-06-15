24536e3ab130ac31db107315602c84509b614d83
Ashutosh Das <dasashutosh@microsoft.com>
2026-06-15 11:00:49 +0530

    Adding web_run_minwin and removing web_run_mincp

--- scenarios/windows/_library/web/web_run_minwin/web_run_minwin.py
+++ scenarios/windows/_library/web/web_run_minwin/web_run_minwin.py
@@ -1,8 +1,8 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import scenarios.app_scenario
-from parameters import Params
+import core.app_scenario
+from core.parameters import Params
 import logging
 import os
 from . import default_params
@@ -10,7 +10,7 @@ from . import default_params
 # Description:
 #   Automatically generated standard scenario.
 
-class WebRunMincp(scenarios.app_scenario.Scenario):
+class WebRunMinwin(core.app_scenario.Scenario):
     # Set default parameters:
     default_params.run()
 
@@ -18,7 +18,7 @@ class WebRunMincp(scenarios.app_scenario.Scenario):
 
     def setUp(self):
         # Load actions JSON.
-        actions_json = os.path.join(os.path.dirname(__file__), "web_run_mincp.json")
+        actions_json = os.path.join(os.path.dirname(__file__), "web_run_minwin.json")
         self.actions = self.load_action_json(actions_json)
 
         # Execute Setup actions, if they exist
@@ -27,7 +27,7 @@ class WebRunMincp(scenarios.app_scenario.Scenario):
             self.run_actions(setup_action["children"])
 
         # Call base class setUp() to dump config, call tool callbacks, and start measurment
-        scenarios.app_scenario.Scenario.setUp(self)
+        core.app_scenario.Scenario.setUp(self)
 
 
     def runTest(self):
@@ -36,7 +36,7 @@ class WebRunMincp(scenarios.app_scenario.Scenario):
         if runtest_action is not None:
             self.run_actions(runtest_action["children"])
             return
-        
+
         # If no "Run Test", "Setup", or "Teardown" specified, then just execute the whole list
         setup_action = self._find_next_type("Setup", json=self.actions)
         teardown_action = self._find_next_type("Teardown", json=self.actions)
@@ -46,7 +46,7 @@ class WebRunMincp(scenarios.app_scenario.Scenario):
 
     def tearDown(self):
         # Call base class tearDown() to stop measurment, copy back data from DUT, and call tool callbacks
-        scenarios.app_scenario.Scenario.tearDown(self)
+        core.app_scenario.Scenario.tearDown(self)
 
         # Execute Teardown actions, if they exist
         teardown_action = self._find_next_type("Teardown", json=self.actions)
@@ -56,5 +56,4 @@ class WebRunMincp(scenarios.app_scenario.Scenario):
 
     def kill(self):
         # In case of scenario failure or termination, kill any applications left open here:
-
         return
