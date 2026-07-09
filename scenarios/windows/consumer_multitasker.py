99d067bdf36610dff597500a22b834800c5b80b3
a5huto5h <ashutoshdas540@gmail.com>
2026-07-09 10:34:25 +0530

    Fixing import in consumer_multitasker

--- scenarios/windows/consumer_multitasker.py
+++ scenarios/windows/consumer_multitasker.py
@@ -1,10 +1,11 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import scenarios.windows.enterprise_collab
 from parameters import Params
 Params.setParam("enterprise_collab", "minwin_workloads", "productivity file_explorer")
 Params.setParam("enterprise_collab", "simple_office_launch", "0")
-import scenarios.windows.mincp_base
+
 class MinCP_Workload_All(scenarios.windows.mincp_base.MincpBase):
     '''
     Microsoft Teams video call with 9 bot participants.
