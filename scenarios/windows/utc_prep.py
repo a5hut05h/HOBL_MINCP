598702966edde2b3509ac00c98a61e8113c0ee39
aadityaaren <izukuu.midoriya.23@gmail.com>
2026-08-12 15:53:53 +0530

    Delete Big XML, does not contain required PT events

--- scenarios/windows/utc_prep.py
+++ scenarios/windows/utc_prep.py
@@ -20,8 +20,8 @@ class UtcPrep(core.app_scenario.Scenario):
         # C:\hobl\HOBL_MINCP\utilities\proprietary\ParseUtc\ConsumerMultitaskerPTs.xml
         # C:\Users\aadityaaren\Downloads\downloadedFile_allnew.xml
         if self.enable == '1':
-            self._upload("utilities\\proprietary\\ParseUtc\\downloadedFile_allnew.xml", sideload_dir)
-            self._call(["cmd.exe", f'/C copy /Y "{sideload_dir}\\downloadedFile_allnew.xml" "{sideload_dir}\\UtcPerftrack.xml"'])
+            self._upload("utilities\\proprietary\\ParseUtc\\ConsumerMultitaskerPTs.xml", sideload_dir)
+            self._call(["cmd.exe", f'/C copy /Y "{sideload_dir}\\ConsumerMultitaskerPTs.xml" "{sideload_dir}\\UtcPerftrack.xml"'])
             self._upload("utilities\\proprietary\\ParseUtc\\DisableAllUploads.json", sideload_dir)
             self._call(["cmd.exe", '/C reg add "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\DataCollection" /v AllowTelemetry /t REG_DWORD /d 3 /f > null 2>&1'])
             self._call(["cmd.exe", '/C reg add "HKLM\\SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting" /v DisableWerUpload /t REG_DWORD /d 1 /f > null 2>&1'])
