93c804e4a060f9d41ace04509ea256d4089527e7
aadityaaren <izukuu.midoriya.23@gmail.com>
2026-08-31 20:13:30 +0530

    Fix _library: prod_outlook_new_attachfile

--- scenarios/windows/_library/productivity/prod_outlook_new_atttachfile/default_params.py
+++ scenarios/windows/_library/productivity/prod_outlook_new_atttachfile/default_params.py
@@ -7,6 +7,7 @@ from utilities.open_source.modules import import_run_user_only
 def run():
     Params.setCalculated('scenario_section', __package__.split('.')[-1])
     run_user_only()
+    Params.setDefault('prod_outlook_new_atttachfile', 'file_source', 'Local', desc='Choose whether to attach the file from OneDrive or the local user profile', valOptions=['OneDrive', 'Local'])
     Params.setDefault('prod_outlook_new_atttachfile', 'file_path', r'abl_docs\asample.pptx', desc='Relative path of the file to attach', valOptions=[r'abl_docs\asample.pptx', r'abl_docs\FamilyBudget.xlsx', r'abl_docs\test_long_doc.docx'])
     return
 
