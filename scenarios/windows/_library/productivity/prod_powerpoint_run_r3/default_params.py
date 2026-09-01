b4a61a22ae90fd349bfcfc45208d08ccd39b43ff
aadityaaren <izukuu.midoriya.23@gmail.com>
2026-09-01 16:30:25 +0530

    Fix _library: prod_powerpoint_run_r3

--- scenarios/windows/_library/productivity/prod_powerpoint_run_r3/default_params.py
+++ scenarios/windows/_library/productivity/prod_powerpoint_run_r3/default_params.py
@@ -8,7 +8,6 @@ from utilities.open_source.modules import import_run_user_only
 def run():
     Params.setCalculated('scenario_section', __package__.split('.')[-1])
     run_user_only()
-    Params.setDefault('prod_powerpoint_run_r3', 'pdf_path', r'\abl_docs\sample.pdf', desc='Path of the PDF to export relative to the user profile', valOptions=[r'\abl_docs\sample.pdf'])
     return
 
 
