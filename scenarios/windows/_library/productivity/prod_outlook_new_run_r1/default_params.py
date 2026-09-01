ee9635ceed8eb3c0c4ee5247316bbfade4a659d4
aadityaaren <izukuu.midoriya.23@gmail.com>
2026-09-01 13:23:25 +0530

    Fix _library: prod_outlook_new_run_r1/2

--- scenarios/windows/_library/productivity/prod_outlook_new_run_r1/default_params.py
+++ scenarios/windows/_library/productivity/prod_outlook_new_run_r1/default_params.py
@@ -10,6 +10,6 @@ def run():
     return
 
 def run_user_only():
-    import_run_user_only('scenarios\\windows\\_library\\productivity\\prod_outlook_new_atttachfile')
+    import_run_user_only('scenarios\\windows\\_library\\productivity\\prod_outlook_new_attachfile')
     import_run_user_only('scenarios\\windows\\_library\\productivity\\prod_outlook_new_switchto')
     return
