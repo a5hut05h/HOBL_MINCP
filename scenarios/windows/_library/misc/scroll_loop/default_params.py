cd9311fb8d3abea3f5293edd5f6b3252f7231a28
a5huto5h <ashutoshdas540@gmail.com>
2026-09-02 13:46:59 +0530

    Adding scroll loop library

---
+++ scenarios/windows/_library/misc/scroll_loop/default_params.py
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+from core.parameters import Params
+from utilities.open_source.modules import import_run_user_only
+
+def run():
+    Params.setCalculated('scenario_section', __package__.split('.')[-1])
+    run_user_only()
+    Params.setDefault('scroll_loop', 'up_count', '1', desc='', valOptions=[])
+    Params.setDefault('scroll_loop', 'down_count', '1', desc='', valOptions=[])
+    return
+
+def run_user_only():
+    return
