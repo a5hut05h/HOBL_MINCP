ffee94067d9e7e2e1cc5425cf06c53eae5903c3b
a5huto5h <ashutoshdas540@gmail.com>
2026-09-08 13:07:52 +0530

    Adding ConsumerMultitasker wrapper script

--- scenarios/windows/consumer_multitasker.py
+++ scenarios/windows/consumer_multitasker.py
@@ -1,14 +1,10 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import scenarios.windows.enterprise_collab
+import scenarios.windows.cm_base
 from parameters import Params
-Params.setParam("enterprise_collab", "minwin_workloads", "productivity file_explorer")
-Params.setParam("enterprise_collab", "simple_office_launch", "0")
+Params.setParam("cm_base", "minwin_workloads", "productivity file_explorer")
 
-class MinCP_Workload_All(scenarios.windows.enterprise_collab.EnterpriseCollab):
-    '''
-    Microsoft Teams video call with 9 bot participants.
-    Local camera and mic are on, other 9 participants are bots sending video and audio.
-    '''
+class MinCP_Workload_All(scenarios.windows.cm_base.CmBase):
+    
     pass
