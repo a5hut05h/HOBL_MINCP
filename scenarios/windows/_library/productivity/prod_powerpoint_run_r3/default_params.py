94023d0dbec97ca4e9474bcf23fbed46b61b60c1
aadityaaren <izukuu.midoriya.23@gmail.com>
2026-09-14 18:17:10 +0530

    Fix _library: prod_powerpoint_run_r2/3, include prod_powerpoint_insertnewslide before pasting and insertpicture

--- scenarios/windows/_library/productivity/prod_powerpoint_run_r3/default_params.py
+++ scenarios/windows/_library/productivity/prod_powerpoint_run_r3/default_params.py
@@ -14,5 +14,6 @@ def run():
 def run_user_only():
     import_run_user_only('scenarios\\windows\\_library\\misc\\recording_phase_begin')
     import_run_user_only('scenarios\\windows\\_library\\misc\\recording_phase_end')
+    import_run_user_only('scenarios\\windows\\_library\\productivity\\prod_powerpoint_insertnewslide')
     import_run_user_only('scenarios\\windows\\_library\\productivity\\prod_powerpoint_switchto')
     return
